6f92d9e84bb58f83a089ffa7a5a028958e941d39
Slava <101575360+ChernoSlava@users.noreply.github.com>
2023-12-04 11:39:16 +0200

    Update index.js
    delete useless code

--- index.js
+++ index.js
@@ -80,53 +80,7 @@ const server = http.createServer( async(req, res) => {
         const responseBody = await response.json();
         const quote = responseBody.quote;
 
-        // const html = generateHTML(quote);
-
-        // res.end(`
-        //     <!DOCTYPE html>
-        //     <html lang="en">
-        //     <head>
-        //         <meta charset="UTF-8">
-        //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //         <title>Kanye Quote</title>
-        //         <style>
-        //             body {
-        //                 font-family: 'Arial', sans-serif;
-        //                 background-color: #f5f5f5;
-        //                 margin: 0;
-        //                 padding: 0;
-        //                 display: flex;
-        //                 align-items: center;
-        //                 justify-content: center;
-        //                 height: 100vh;
-        //             }
-        //             .quote-container {
-        //                 text-align: center;
-        //                 padding: 20px;
-        //                 border-radius: 8px;
-        //                 background-color: #fff;
-        //                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        //             }
-        //             h1 {
-        //                 font-size: 24px;
-        //                 color: #333;
-        //                 margin-bottom: 10px;
-        //             }
-        //             .quote-text {
-        //                 font-size: 18px;
-        //                 color: #555;
-        //             }
-        //         </style>
-        //     </head>
-        //     <body>
-        //         <div class="quote-container">
-        //             <h1>Quote</h1>
-        //             <p class="quote-text">${quote}</p>
-        //         </div>
-        //     </body>
-        //     </html>
-        // `, 'utf8');
-    res.end(generateHTML(quote), 'utf8');
+        res.end(generateHTML(quote), 'utf8');
     } catch (error) {
         console.error('Error fetching data:', error);
         res.end('<h1>Nothing</h1>', 'utf8');

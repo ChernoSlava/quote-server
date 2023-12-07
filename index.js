651b31936ebbe46d362e14f04e077846969d1245
Slava <101575360+ChernoSlava@users.noreply.github.com>
2023-12-07 11:35:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,79 @@
 const http = require('http');
 
-function generateHTML(quote) {
+function getStyles() {
     return `
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+        .quote-container {
+            text-align: center;
+            padding: 20px;
+            border-radius: 8px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+            margin-bottom: 20px;
+        }
+        h1 {
+            font-size: 24px;
+            color: #333;
+            margin-bottom: 10px;
+        }
+        .quote-text {
+            font-size: 18px;
+            color: #555;
+        }
+        .reload-button {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    `;
+}
+
+function generateHTML(quote) {
+    if (typeof quote === 'string') {
+
+        return `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Kanye Quote</title>
+                <style>
+                    ${getStyles()}
+                </style>
+            </head>
+            <body>
+                <div class="quote-container">
+                    <h1>Quote</h1>
+                    <p class="quote-text">${quote}</p>
+                </div>
+                <button class="reload-button">Next Quote</button>
+
+                <script>
+                    const reload = () => location.reload();
+                    const nextQuote = document.querySelector('.reload-button').addEventListener('click', reload);
+                </script>
+            </body>
+            </html>
+        `;
+    } else {
+        console.log('quote isn"t string', quote );
+
+        return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
@@ -9,54 +81,23 @@ function generateHTML(quote) {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Kanye Quote</title>
             <style>
-                body {
-                    font-family: 'Arial', sans-serif;
-                    background-color: #f5f5f5;
-                    margin: 0;
-                    padding: 0;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    height: 100vh;
-                }
-                .quote-container {
-                    text-align: center;
-                    padding: 20px;
-                    border-radius: 8px;
-                    background-color: #fff;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-                    margin-bottom: 20px;
-                }
-                h1 {
-                    font-size: 24px;
-                    color: #333;
-                    margin-bottom: 10px;
-                }
-                .quote-text {
-                    font-size: 18px;
-                    color: #555;
-                }
-                .reload-button {
-                    padding: 10px 20px;
-                    font-size: 16px;
-                    background-color: #4CAF50;
-                    color: white;
-                    border: none;
-                    border-radius: 4px;
-                    cursor: pointer;
-                }
+                ${getStyles()}
             </style>
         </head>
         <body>
             <div class="quote-container">
-                <h1>Quote</h1>
-                <p class="quote-text">${quote}</p>
+                <h1>Something went wrong</h1>
             </div>
-            <button class="reload-button" onclick="location.reload()">Next Quote</button>
+            <script>
+                const reload = () => location.reload();
+                const nextQuote = document.querySelector('.reload-button').addEventListener('click', reload);
+            </script>
         </body>
         </html>
     `;
+    }
+
+
 };
 
 const server = http.createServer( async(req, res) => {
@@ -65,18 +106,13 @@ const server = http.createServer( async(req, res) => {
     });
 
     try {
-
         const fetch = (await import('node-fetch')).default;
         const response = await fetch('https://api.kanye.rest');
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
-        console.log(response);
-        // const responseBody = await response.text();
-        // console.log(responseBody.quote)
-
-        // res.end('<h1>${responseBody}</h1>', 'utf8');
         const responseBody = await response.json();
         const quote = responseBody.quote;
 
@@ -88,4 +124,5 @@ const server = http.createServer( async(req, res) => {
 });
 
 server.listen(3000);  
+
 
